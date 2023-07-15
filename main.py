e3cfbb654690aaf1805a0aed63f46e5b27e08011
Kan Fukuroi <kan.fukuroi@aillis.jp>
2023-07-16 01:14:07 +0900

    バリデーションを追加

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-from typing import Optional
+from typing import Optional, List
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+
+class shopInfo(BaseModel):
+    name: str
+    location: str
 
 
 class Item(BaseModel):
-    name: str
+    name: str = Field(min_length=4, max_length=12)
     description: Optional[str] = None
     price: int
     tax: Optional[float] = None
 
 
+class Data(BaseModel):
+    shop_info: Optional[shopInfo] = None
+    items: List[Item]
+
+
 app = FastAPI()
+
+
+@app.post("/")
+async def index(data: Data):
+    return {"data": data}
 
 
 @app.post("/item/")
