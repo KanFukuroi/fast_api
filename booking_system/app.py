2542411268035253eed92b06d0f3b06194d0d3be
Kan Fukuroi <kan.fukuroi@aillis.jp>
2023-07-23 23:13:15 +0900

    予約画面

--- booking_system/app.py
+++ booking_system/app.py
@@ -1,9 +1,10 @@
 import streamlit as st
+import datetime
 import random
 import requests
 import json
 
-page = st.sidebar.selectbox("Choose your page", ["users", "rooms"])
+page = st.sidebar.selectbox("Choose your page", ["users", "rooms", "bookings"])
 
 if page == "users":
     st.title("APIテスト画面(ユーザー)")
@@ -25,7 +26,7 @@ if page == "users":
 elif page == "rooms":
     st.title("APIテスト画面(会議室)")
 
-    with st.form(key="user"):
+    with st.form(key="room"):
         room_id: int = random.randint(0, 10)
         room_name: str = st.text_input("会議室名", max_chars=12)
         capacity: int = st.number_input("定員", step=1)
@@ -37,6 +38,47 @@ elif page == "rooms":
         st.json(data)
         st.write("## レスポンスデータ")
         url = "http://127.0.0.1:8000/rooms"
+        res = requests.post(url, data=json.dumps(data))
+        st.write(res.status_code)
+        st.json(res.json())
+elif page == "bookings":
+    st.title("APIテスト画面(予約)")
+
+    with st.form(key="booking"):
+        booking_id: int = random.randint(0, 10)
+        user_id: int = random.randint(0, 10)
+        room_id: int = random.randint(0, 10)
+        booked_num: int = st.number_input("予約人数", step=1)
+        date = st.date_input("日付: ", min_value=datetime.date.today())
+        start_time = st.time_input("開始時刻: ", value=datetime.time(hour=9, minute=0))
+        end_time = st.time_input("終了時刻: ", value=datetime.time(hour=20, minute=0))
+        submit_button = st.form_submit_button(label="リクエスト送信")
+        data = {
+            "booking_id": booking_id,
+            "user_id": user_id,
+            "room_id": room_id,
+            "booked_num": booked_num,
+            "start_datetime": datetime.datetime(
+                year=date.year,
+                month=date.month,
+                day=date.day,
+                hour=start_time.hour,
+                minute=start_time.minute,
+            ).isoformat(),
+            "end_datetime": datetime.datetime(
+                year=date.year,
+                month=date.month,
+                day=date.day,
+                hour=end_time.hour,
+                minute=end_time.minute,
+            ).isoformat(),
+        }
+
+    if submit_button:
+        st.write("## リクエストデータ")
+        st.json(data)
+        st.write("## レスポンスデータ")
+        url = "http://127.0.0.1:8000/bookings"
         res = requests.post(url, data=json.dumps(data))
         st.write(res.status_code)
         st.json(res.json())
